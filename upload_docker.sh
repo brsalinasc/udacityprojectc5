032c98d39bac29afb3633c67688924727efe2d05
Brian Salinas <br.salinasc@gmail.com>
2020-05-05 18:00:34 -0400

    modificacion upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=bcsalinasc/bcsalinasc-app:v0.1.0
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username bcsalinasc
+docker tag bcsalinasc-app:v0.1.0 $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
