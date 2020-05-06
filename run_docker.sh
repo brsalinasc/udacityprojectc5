8321a867b6ba9dc2343f0bf47aaad9b9028259ca
Brian Salinas <br.salinasc@gmail.com>
2020-05-06 13:43:23 -0400

    cambios hasta tarea 7

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t bcsalinasc-app:v0.1.0 .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 bcsalinasc-app:v0.1.0
